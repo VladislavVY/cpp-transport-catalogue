8a10e5f65f3800585b34170596a3ba24e23cb924
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-02-26 21:26:39 +0300

    input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,1 +1,110 @@
-// место для вашего кода
+#include "input_reader.h"
+
+#include <algorithm>
+#include <cassert>
+#include <iterator>
+#include <string>
+#include <vector>
+#include <cmath>
+
+namespace transport {
+
+/**
+ * Парсит строку вида "10.123,  -30.1837" и возвращает пару координат (широта, долгота)
+ */
+geo::Coordinates ParseCoordinates(std::string_view str) {
+    static const double nan = std::nan("");
+
+    auto not_space = str.find_first_not_of(' ');
+    auto comma = str.find(',');
+
+    if (comma == str.npos) {
+        return {nan, nan};
+    }
+
+    auto not_space2 = str.find_first_not_of(' ', comma + 1);
+
+    double lat = std::stod(std::string(str.substr(not_space, comma - not_space)));
+    double lng = std::stod(std::string(str.substr(not_space2)));
+
+    return {lat, lng};
+}
+
+/**
+ * Удаляет пробелы в начале и конце строки
+ */
+std::string_view Trim(std::string_view string) {
+    const auto start = string.find_first_not_of(' ');
+    if (start == string.npos) {
+        return {};
+    }
+    return string.substr(start, string.find_last_not_of(' ') + 1 - start);
+}
+
+/**
+ * Разбивает строку string на n строк, с помощью указанного символа-разделителя delim
+ */
+std::vector<std::string_view> Split(std::string_view string, char delim) {
+    std::vector<std::string_view> result;
+
+    size_t pos = 0;
+    while ((pos = string.find_first_not_of(' ', pos)) < string.length()) {
+        auto delim_pos = string.find(delim, pos);
+        if (delim_pos == string.npos) {
+            delim_pos = string.size();
+        }
+        if (auto substr = Trim(string.substr(pos, delim_pos - pos)); !substr.empty()) {
+            result.push_back(substr);
+        }
+        pos = delim_pos + 1;
+    }
+
+    return result;
+}
+
+/**
+ * Парсит маршрут.
+ * Для кольцевого маршрута (A>B>C>A) возвращает массив названий остановок [A,B,C,A]
+ * Для некольцевого маршрута (A-B-C-D) возвращает массив названий остановок [A,B,C,D,C,B,A]
+ */
+std::vector<std::string> ParseRoute(std::string_view route) {   
+    if (route.find('>') != route.npos) {
+        auto stops = Split(route, '>');
+        std::vector<std::string> route_stops(stops.begin(), stops.end());
+        return route_stops;
+    }
+    auto stops = Split(route, '-');
+        std::vector<std::string_view> results(stops.begin(), stops.end());
+        results.insert(results.end(), std::next(stops.rbegin()), stops.rend());
+        std::vector<std::string> route_stops(results.begin(), results.end());
+        return route_stops;
+}
+
+    
+void FillCatalogue(std::istream& input, Catalogue& catalogue) {
+    std::vector<std::string> query_bus;
+    size_t requests_count;
+    input >> requests_count;
+    for (size_t i = 0; i < requests_count; ++i) {
+        std::string command, line;
+        input >> command; 
+        if (command == "Stop") {
+            std::getline(input, line);
+            std::string stop_name = line.substr(1, line.find_first_of(':') - line.find_first_of(' ') - 1);
+            line.erase(1, line.find_first_of(':'));
+            geo::Coordinates stop_coordinates = ParseCoordinates(line);
+            catalogue.AddStop(std::move(stop_name), stop_coordinates);
+        }
+        if (command == "Bus") {
+            std::getline(input, line);
+            query_bus.push_back(line);
+        }
+    }
+    for (auto& bus : query_bus) {
+        std::string route_number = bus.substr(1, bus.find_first_of(':') - 1);
+        bus.erase(0, bus.find_first_of(':') + 2);    
+        auto route_stops = ParseRoute(bus);
+        catalogue.AddBus(std::move(route_number), std::move(route_stops));
+    }
+}
+}
