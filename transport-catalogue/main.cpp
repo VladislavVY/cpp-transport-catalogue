3049fd3595e55bdeced2f299b1233294fd91935e
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-02-26 21:28:10 +0300

    main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,10 @@
-// место для вашего кода
+#include "stat_reader.h"
+#include "input_reader.h"
+
+using namespace transport;
+
+int main() {
+    Catalogue transport_catalogue;
+    FillCatalogue(std::cin, transport_catalogue);
+    ParseAndPrintStat(transport_catalogue, std::cout);
+}
