eaf07941bb983b248af39ef0e0cf8b64cac1b4ab
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-02-26 21:29:10 +0300

    stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,11 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+#include <iostream>
+
+namespace transport {
+
+void ParseAndPrintStat(const Catalogue& transport_catalogue, std::ostream& output);
+    
+}
