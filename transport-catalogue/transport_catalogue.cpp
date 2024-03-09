ba505c2a9f06fc2bee70a6e21e98706afcf00f53
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-03-09 22:11:32 +0300

    transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -16,10 +16,15 @@ const Stop* Catalogue::FindStop(const std::string& stop_name) const {
     
     
 void Catalogue::AddBus(const std::string& route_number, const std::vector<std::string>& route_stops) {
-    buses_.push_back({route_number, route_stops});
-    for (const auto& route_stop : route_stops) {
+    std::vector<const Stop*> stops;
+	for (auto& stop : route_stops) {
+		const auto& st = FindStop(stop);
+		stops.push_back(st);
+	} 
+	buses_.push_back({std::string(route_number), stops});
+    for (const auto& route_stop : stops) {
         for (auto& stop_ : stops_) {
-            if (stop_.name == route_stop) stop_.buses.insert(route_number);
+            if (stop_.name == route_stop->name) stop_.buses.insert(std::string(route_number));
         }
     }
     busname_to_bus_[buses_.back().number] = &buses_.back();
@@ -36,18 +41,22 @@ const Bus* Catalogue::FindBus(const std::string& route_number) const {
 const BusInfo Catalogue::GetBusInfo(const std::string& route_number) const {
     BusInfo bus_info{};
     const Bus* bus = FindBus(route_number);
-    bus_info.stops_count = bus->stops->size();
+    bus_info.stops_count = bus->stops.size();
     double route_length = 0.0;
-    for (auto iter = bus->stops.value().begin(); iter + 1 != bus->stops.value().end(); ++iter) {
-        route_length += ComputeDistance(stopname_to_stop_.find(*iter)->second->coordinates,
-        stopname_to_stop_.find(*(iter + 1))->second->coordinates);
+    double geographic_length = 0.0;
+    for (size_t i = 0; i < bus->stops.size() - 1; ++i) {
+        auto first = bus->stops[i];
+        auto second = bus->stops[i + 1];
+        route_length += GetStopDistance(first, second);
+        geographic_length += geo::ComputeDistance(first->coordinates, second->coordinates);
     }
-    std::unordered_set<std::string> unique_stops;
-    for (const auto& stop : busname_to_bus_.at(route_number)->stops.value()) {
-        unique_stops.insert(stop);
+    std::unordered_set<std::string_view> unique_stops;
+    for (const auto& stop : busname_to_bus_.at(route_number)->stops) {
+        unique_stops.insert(stop->name);
     }
     bus_info.unique_stops_count = unique_stops.size();
     bus_info.route_length = route_length;
+    bus_info.curvature = route_length / geographic_length;
     return bus_info;
 }
 
@@ -55,4 +64,16 @@ const BusInfo Catalogue::GetBusInfo(const std::string& route_number) const {
 const std::set<std::string> Catalogue::GetBusesOnStop(const std::string& stop_name) const {
     return stopname_to_stop_.at(stop_name)->buses;
 }
+    
+void Catalogue::SetStopDistance(const Stop* first, const Stop* second, const int distance) {
+    stop_distances_[{first, second}] = distance;
+}
+
+int Catalogue::GetStopDistance(const Stop* first, const Stop* second) const {
+    if (stop_distances_.count({first, second})){        
+        return stop_distances_.at({first, second});
+    } else if (stop_distances_.count({second, first})){        
+        return stop_distances_.at({second, first});
+    } else return 0;
+}    
 }
