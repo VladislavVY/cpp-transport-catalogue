d5180198d90d7b5cc591f4be0b3782de46f6ce3a
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-03-24 00:27:50 +0300

    geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -15,14 +15,6 @@ struct Coordinates {
     }
 };
 
-inline double ComputeDistance(Coordinates from, Coordinates to) {
-    using namespace std;
-    const int earth_radius = 6371000;
-    if (from == to) {
-        return 0;
-    }
-    static const double dr = 3.1415926535 / 180.;
-    return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr)) * earth_radius;
-}
-}
+double ComputeDistance(Coordinates from, Coordinates to);
+
+} // namespace geo
