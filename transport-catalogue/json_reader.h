f1e8996ebe70bc4718201b3c7a85babad43aabd5
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-03-24 00:31:05 +0300

    json_reader.h

---
+++ transport-catalogue/json_reader.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "json.h"
+#include "transport_catalogue.h"
+#include "map_renderer.h"
+#include "request_handler.h"
+
+#include <iostream>
+
+class JsonReader {
+public:
+    JsonReader(std::istream& input)
+        : input_(json::Load(input))
+    {}
+
+    const json::Node& GetStatRequests() const;
+    const json::Node& GetRenderSettings() const;
+
+
+    void FillCatalogue(transport::Catalogue& catalogue);
+    renderer::MapRenderer FillRenderSettings(const json::Dict& map_request) const;
+    void SetBasicSettings(renderer::RenderSettings& settings, const json::Dict& map_request) const;
+    void SetLabelSettings(renderer::RenderSettings& settings, const json::Dict& map_request) const;
+    void SetUnderlayerColor(renderer::RenderSettings& settings, const json::Dict& map_request) const;
+    void SetColorPalette(renderer::RenderSettings& settings, const json::Dict& v) const;
+    svg::Color ParseColor(const json::Node& color_node) const;
+
+    void PrintStatRequests(const json::Node& stat_requests, RequestHandler& rh) const;
+    const json::Node PrintRoute(const json::Dict& map_request, RequestHandler& rh) const;
+    const json::Node PrintStop(const json::Dict& map_request, RequestHandler& rh) const;
+    const json::Node PrintMap(const json::Dict& map_request, RequestHandler& rh) const;
+
+private:
+    json::Document input_;
+    json::Node nul_ = nullptr;
+
+    std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> FillStop(const json::Dict& map_request) const;
+   void FillStopDistances(transport::Catalogue& catalogue, const json::Dict& map_request) const;
+    std::tuple<std::string_view, std::vector<const transport::Stop*>, bool> FillRoute(const json::Dict& map_request, transport::Catalogue& catalogue) const;
+};
