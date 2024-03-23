fc49c0a536debeed54fb0db50e4d898a94b67802
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-03-24 00:32:28 +0300

    main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,10 +1,17 @@
-#include "stat_reader.h"
-#include "input_reader.h"
-
-using namespace transport;
+#include "json_reader.h"
+#include "request_handler.h"
 
 int main() {
-    Catalogue transport_catalogue;
-    FillCatalogue(std::cin, transport_catalogue);
-    ParseAndPrintStat(transport_catalogue, std::cout);
+    transport::Catalogue catalogue;
+    JsonReader json_doc(std::cin);
+    
+    json_doc.FillCatalogue(catalogue);
+    
+    const auto& stat_requests = json_doc.GetStatRequests();
+    const auto& render_settings = json_doc.GetRenderSettings().AsMap();
+    const auto& renderer = json_doc.FillRenderSettings(render_settings);
+    
+    RequestHandler req_hand(catalogue, renderer);
+    
+    json_doc.PrintStatRequests(stat_requests, req_hand);
 }
