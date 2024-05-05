3788df4c1cb0962827bcdc952a096cba6d3ca10a
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-05-05 23:31:46 +0300

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -5,37 +5,37 @@
 
 #include <memory>
 
+    
 namespace transport {
 
 constexpr static double K_MH_TO_M_MIN = 1000.0 / 60.0;
     
 class TransportRouter {
-public:
     
-	TransportRouter() = default;
+public:
+    struct Settings {
+        int bus_wait_time = 0;
+        double bus_velocity = 0.0;
+    };
 
-	TransportRouter(const int bus_wait_time, const double bus_velocity)
-		: bus_wait_time_(bus_wait_time)
-		, bus_velocity_(bus_velocity) 
-        {
-        }
+    TransportRouter() = default;
 
-	TransportRouter(const TransportRouter& settings, const Catalogue& catalogue) {
-		bus_wait_time_ = settings.bus_wait_time_;
-		bus_velocity_ = settings.bus_velocity_;
-		BuildGraph(catalogue);
-	}
-
-	const graph::DirectedWeightedGraph<double>& BuildGraph(const Catalogue& catalogue);
-    void BuildStopsGraph(const Catalogue& catalogue);
-    void BuildBusesGraph(const Catalogue& catalogue);
-	const std::optional<graph::Router<double>::RouteInfo> GetRouter(const std::string_view stop_from, const std::string_view stop_to) const;
-	const graph::DirectedWeightedGraph<double>& GetGraph() const;
+    explicit TransportRouter(const Settings& settings, const Catalogue& catalogue)
+        : settings_(settings) {
+        BuildGraph(catalogue);
+    }
+    
+	using RouteInfo = graph::Router<double>::RouteInfo;
+    const std::optional<RouteInfo> FindRoute(const std::string_view stop_from, const std::string_view stop_to) const;
+    
+	const graph::DirectedWeightedGraph<double>& GetGraph() const; //используется в GetRouterGraph() request_handler.cpp
 
 private:
-	int bus_wait_time_ = 0;
-	double bus_velocity_ = 0.0;
-
+    Settings settings_;
+    const graph::DirectedWeightedGraph<double>& BuildGraph(const Catalogue& catalogue);
+    void BuildStopsGraph(const Catalogue& catalogue);
+    void BuildBusesGraph(const Catalogue& catalogue);
+    
 	graph::DirectedWeightedGraph<double> graph_;
 	std::map<std::string, graph::VertexId> stop_id_;
 	std::unique_ptr<graph::Router<double>> router_;
