98d7ddf2d02eff5fcb027e5d81a0a154d61c3ddc
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-05-05 18:10:51 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -8,10 +8,13 @@ int main() {
     json_doc.FillCatalogue(catalogue);
     
     const auto& stat_requests = json_doc.GetStatRequests();
-    const auto& render_settings = json_doc.GetRenderSettings().AsMap();
+    const auto& render_settings = json_doc.GetRenderSettings();
+    const auto& routing_settings = json_doc.GetRoutingSettings();
     const auto& renderer = json_doc.FillRenderSettings(render_settings);
+    const auto& full_routing = json_doc.FillRoutingSettings(routing_settings);
+    const transport::TransportRouter router = {full_routing, catalogue};
     
-    RequestHandler req_hand(catalogue, renderer);
+    RequestHandler req_hand(catalogue, renderer, router);
     
     json_doc.PrintStatRequests(stat_requests, req_hand);
 }
