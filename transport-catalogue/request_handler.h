951a5e25913f8b244f42ed27e59a8e1092bc452d
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-05-05 18:12:18 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -1,16 +1,18 @@
 #pragma once
 
 #include "json.h"
-#include "transport_catalogue.h"
 #include "map_renderer.h"
+#include "transport_catalogue.h"
+#include "transport_router.h"
 
 #include <sstream>
 
 class RequestHandler {
 public:
-    RequestHandler(const transport::Catalogue& catalogue, const renderer::MapRenderer& renderer)
+    RequestHandler(const transport::Catalogue& catalogue, const renderer::MapRenderer& renderer, const transport::TransportRouter& router)
         : catalogue_(catalogue)
         , renderer_(renderer)
+        , router_(router)
     {
     }
 
@@ -19,8 +21,11 @@ public:
     const std::set<std::string> GetBusesOnStop(std::string_view stop_name) const;
     bool SearchBusNumber(const std::string_view bus_number) const;
     bool SearchStopName(const std::string_view stop_name) const;
+    const std::optional<graph::Router<double>::RouteInfo> GetRouting(const std::string_view stop_name_from, const std::string_view stop_name_to) const;
+    const graph::DirectedWeightedGraph<double>& GetGraph() const;
 
 private:
     const transport::Catalogue& catalogue_;
     const renderer::MapRenderer& renderer_;
+    const transport::TransportRouter& router_;
 };
