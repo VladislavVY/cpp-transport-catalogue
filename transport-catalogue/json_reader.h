4070f9bd6d8ce658a3e3644a79dfc89c9743b576
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-05-05 18:11:19 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include "json.h"
-#include "transport_catalogue.h"
 #include "map_renderer.h"
 #include "request_handler.h"
+#include "transport_catalogue.h"
 
 #include <iostream>
 
@@ -15,26 +15,30 @@ public:
 
     const json::Node& GetStatRequests() const;
     const json::Node& GetRenderSettings() const;
+    const json::Node& GetRoutingSettings() const;
 
 
     void FillCatalogue(transport::Catalogue& catalogue);
-    renderer::MapRenderer FillRenderSettings(const json::Dict& map_request) const;
+    renderer::MapRenderer FillRenderSettings(const json::Node& settings) const;
     void SetBasicSettings(renderer::RenderSettings& settings, const json::Dict& map_request) const;
     void SetLabelSettings(renderer::RenderSettings& settings, const json::Dict& map_request) const;
     void SetUnderlayerColor(renderer::RenderSettings& settings, const json::Dict& map_request) const;
     void SetColorPalette(renderer::RenderSettings& settings, const json::Dict& v) const;
     svg::Color ParseColor(const json::Node& color_node) const;
+    transport::TransportRouter FillRoutingSettings(const json::Node& settings) const;
 
     void PrintStatRequests(const json::Node& stat_requests, RequestHandler& rh) const;
     const json::Node PrintRoute(const json::Dict& map_request, RequestHandler& rh) const;
     const json::Node PrintStop(const json::Dict& map_request, RequestHandler& rh) const;
     const json::Node PrintMap(const json::Dict& map_request, RequestHandler& rh) const;
+    const json::Node CreateRouteItem(const double& time, const std::string& type) const;
+    const json::Node PrintRouting(const json::Dict& map_request, RequestHandler& rh) const;
 
 private:
     json::Document input_;
     json::Node nul_ = nullptr;
 
     std::tuple<std::string_view, geo::Coordinates, std::map<std::string_view, int>> FillStop(const json::Dict& map_request) const;
-   void FillStopDistances(transport::Catalogue& catalogue, const json::Dict& map_request) const;
+    void FillStopDistances(transport::Catalogue& catalogue, const json::Dict& map_request) const;
     std::tuple<std::string_view, std::vector<const transport::Stop*>, bool> FillRoute(const json::Dict& map_request, transport::Catalogue& catalogue) const;
 };
